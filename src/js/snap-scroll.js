cd2e27e0ce23cc4f3da50d0d17337eb839d93588
AJ Savino <aj.savino@moxieusa.com>
2018-06-13 17:14:25 -0400

    added visible / active classes to sections while scrolling / snapping

--- src/js/snap-scroll.js
+++ src/js/snap-scroll.js
@@ -1,20 +1,23 @@
 /*
-* AJ Savino
-* Moxie - 6/7/2018
-*
-* Javascript library to snap to page sections while scrolling and with mouse wheel
+* SnapScroll v1.1.0 Copyright (c) 2018 AJ Savino
 * https://github.com/koga73/SnapScroll
+* MIT License
 */
 (function($){
 	$.fn.SnapScroll = function(options){
 		var _instance = null;
 
+		//Public
 		var defaults = {
+			useClasses:true,
+			classVisible:"snap-scroll-visible",
+			classActive:"snap-scroll-active",
+
 			scrollDelay:300,		//ms
 			wheelDelay:75,			//ms
 			animateTime:250,		//ms
 			animateTimeBuffer:100,	//ms
-			
+
 			snapTop:true,
 			snapBottom:true,
 			snaps:[]
@@ -24,7 +27,7 @@
 		var _vars = {
 			_$this:this,
 			_resizer:null,
-			
+
 			_snaps:null,
 			_scrollTimeout:0,
 			_wheelTimeout:0,
@@ -37,13 +40,13 @@
 			init:function(){
 				_vars._resizer = new Resizer({onResize:_methods._handler_resize});
 				_methods._handler_resize(); //Call initially
-				
+
 				$(document).on("scroll", _methods._handler_document_scroll);
 				$(document).on("keydown", _methods._handler_document_keydown);
 				$(window).on("DOMMouseScroll mousewheel wheel", _methods._handler_window_mousewheel);
 			},
 
-			destory:function(){
+			destroy:function(){
 				if (_vars._resizer){
 					_vars._resizer.destroy();
 					_vars._resizer = null;
@@ -51,7 +54,7 @@
 				$(document).off("scroll", _methods._handler_document_scroll);
 				$(document).off("keydown", _methods._handler_document_keydown);
 				$(window).off("DOMMouseScroll mousewheel wheel", _methods._handler_window_mousewheel);
-				
+
 				_vars._snaps = null;
 				if (_vars._scrollTimeout){
 					clearTimeout(_vars._scrollTimeout);
@@ -79,37 +82,60 @@
 						closestIndex = i;
 					}
 				}
-				_methods.snapIndex(closestIndex);
+				_instance.snapIndex(closestIndex);
 			},
 
 			snapPrev:function(){
-				_methods.snapIndex(_vars._currentSnapIndex - 1);
+				_instance.snapIndex(_vars._currentSnapIndex - 1);
 			},
 
 			snapNext:function(){
-				_methods.snapIndex(_vars._currentSnapIndex + 1);
+				_instance.snapIndex(_vars._currentSnapIndex + 1);
 			},
 
 			snapIndex:function(index){
 				index = Math.min(Math.max(index, 0), _vars._snaps.length - 1);
 				_vars._currentSnapIndex = index;
-				_methods._scrollTo(_vars._snaps[index]);
+				var snap = _vars._snaps[index];
+
+				//Eval active/visible classes
+				if (_instance.useClasses){
+					var $active = null;
+					_vars._$this.each(function(){
+						var $el = $(this);
+						$el.removeClass(_instance.classActive);
+						if ($active){ //First match
+							return;
+						}
+						if ($el.offset().top == snap){
+							$active = $el;
+						}
+					});
+					if ($active){
+						$active.addClass(_instance.classActive);
+					}
+					_methods._evalVisibility();
+				}
+
+				//Animate
+				_methods._scrollTo(snap);
 			},
 
 			getSnapIndex:function(){
 				return _vars._currentSnapIndex;
 			},
-			
-			updateSnaps:function(){
+
+			//Update snaps
+			update:function(){
 				var snaps = _instance.snaps.concat(); //Copy
 				var pageBottom = $(document).height() - window.innerHeight;
-				
+
 				//Add tags
 				var $tags = _vars._$this;
 				$tags.each(function(){
 					snaps.push($(this).offset().top);
 				});
-				
+
 				//Add top
 				if (_instance.snapTop){
 					snaps.push(0);
@@ -118,7 +144,7 @@
 				if (_instance.snapBottom){
 					snaps.push(pageBottom);
 				}
-				
+
 				//Filter duplicates and below page bottom
 				snaps = snaps.reduce(function(arr, snap){
 					if (arr.indexOf(snap) == -1 && snap <= pageBottom){
@@ -126,19 +152,45 @@
 					}
 					return arr;
 				}, []);
-				
+
 				//Sort
 				snaps.sort(_methods._sortNumeric);
-				
+
 				_vars._snaps = snaps;
 				return _vars._snaps;
 			},
-			
+
+			isVisible:function($el){
+				var elTop = $el.offset().top;
+				var elBottom = elTop + $el.height();
+				var scrollTop = document.documentElement.scrollTop;
+				var scrollBottom = scrollTop + window.innerHeight;
+				if ((elTop >= scrollTop && elTop < scrollBottom) || (elBottom > scrollTop && elBottom <= scrollBottom)){
+					return true;
+				}
+				return false;
+			},
+
+			_evalVisibility:function(){
+				//Toggle visibility class
+				_vars._$this.each(function(){
+					var $el = $(this);
+					if (_instance.isVisible($el)){
+						$el.addClass(_instance.classVisible);
+					} else {
+						$el.removeClass(_instance.classVisible);
+					}
+				});
+			},
+
 			_sortNumeric:function(a, b){
 				return a - b;
 			},
-			
+
 			_handler_document_scroll:function(evt){
+				if (_instance.useClasses){
+					_methods._evalVisibility();
+				}
 				if (_vars._scrollTimeout){
 					clearTimeout(_vars._scrollTimeout);
 				}
@@ -149,8 +201,8 @@
 			_handler_scroll_timeout:function(){
 				clearTimeout(_vars._scrollTimeout);
 				_vars._scrollTimeout = 0;
-				
-				_methods.snapClosest();
+
+				_instance.snapClosest();
 			},
 
 			_scrollTo:function(top){
@@ -159,7 +211,7 @@
 					return;
 				}
 				_vars._lastAnimateTime = new Date().getTime();
-				
+
 				var $htmlBody = $("html,body");
 				$htmlBody.stop(true);
 				$htmlBody.animate({
@@ -169,8 +221,7 @@
 
 			_handler_window_mousewheel:function(evt){
 				evt.preventDefault();
-				
-				_vars._isAnimating = true;
+
 				if (_vars._wheelTimeout){
 					clearTimeout(_vars._wheelTimeout);
 				}
@@ -187,33 +238,34 @@
 				_vars._wheelTimeout = 0;
 
 				if (_vars._wheelDir < 0){
-					_methods.snapPrev();
+					_instance.snapPrev();
 				} else if (_vars._wheelDir > 0){
-					_methods.snapNext();
+					_instance.snapNext();
 				}
 			},
 
 			_handler_document_keydown:function(evt){
 				switch (evt.keyCode){
 					case 38: //Up
-						_methods.snapPrev();
+						_instance.snapPrev();
 						break;
 					case 40: //Down
-						_methods.snapNext();
+						_instance.snapNext();
 						break;
 				}
 			},
-			
+
 			_handler_resize:function(width, height){
-				_methods.updateSnaps();
-				_methods.snapClosest();
+				_instance.update();
+				if (_vars._currentSnapIndex == -1){
+					_instance.snapClosest();
+				} else {
+					_instance.snapIndex(_vars._currentSnapIndex);
+				}
 			}
 		};
 
 		_instance = $.extend({
-			snapTop:_vars.snapTop,
-			snapBottom:_vars.snapBottom,
-
 			init:_methods.init,
 			destroy:_methods.destroy,
 			snapClosest:_methods.snapClosest,
@@ -221,11 +273,18 @@
 			snapNext:_methods.snapNext,
 			snapIndex:_methods.snapIndex,
 			getSnapIndex:_methods.getSnapIndex,
-			updateSnaps:_methods.updateSnaps
+			update:_methods.update,
+			isVisible:_methods.isVisible
 		}, defaults, options);
 		_instance.init();
+		return _instance;
 	};
-	
+
+	/*
+	* Resizer v1.0.2 Copyright (c) 2018 AJ Savino
+	* https://github.com/koga73/Resizer
+	* MIT License
+	*/
 	function Resizer(params){
 		var _instance = null;
 
@@ -233,7 +292,7 @@
 			callbackDelay:300,      //Time in ms to wait before calling onResize
 
 			_lastOrientation:window.orientation,
-			_timeout:null,
+			_timeout:0
 		};
 
 		var _methods = {
@@ -251,7 +310,7 @@
 				var timeout = _vars._timeout;
 				if (timeout){
 					clearTimeout(timeout);
-					_vars._timeout = null;
+					_vars._timeout = 0;
 				}
 				_instance.onResize = null;
 
@@ -275,22 +334,21 @@
 			_handler_resize:function(){
 				if ("onorientationchange" in window){
 					var orientation = window.orientation;
-					if (orientation != _vars._lastOrientation){
-						_vars._lastOrientation = orientation;
-					} else {
+					if (orientation == _vars._lastOrientation){
 						return;
 					}
+					_vars._lastOrientation = orientation;
 				}
-				var timeout = _vars._timeout;
-				if (timeout){
-					clearTimeout(timeout);
-					_vars._timeout = null;
+				if (_vars._timeout){
+					clearTimeout(_vars._timeout);
 				}
-				_vars._timeout = setTimeout(function(){
-					clearTimeout(timeout);
-					_vars._timeout = null;
-					_instance.onResize(_instance.getWidth(), _instance.getHeight());
-				}, _instance.callbackDelay);
+				_vars._timeout = setTimeout(_methods._handler_timeout, _instance.callbackDelay);
+			},
+
+			_handler_timeout:function(){
+				clearTimeout(_vars._timeout);
+				_vars._timeout = 0;
+				_instance.onResize(_instance.getWidth(), _instance.getHeight());
 			}
 		};
 
